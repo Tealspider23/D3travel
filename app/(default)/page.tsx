2f7fe2731a0738afa033c5ae6e6b75d655ad52ea
Tealspider23 <amitbehera204@gmail.com>
2023-10-07 18:34:37 +0530

    commit 2

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,9 +5,8 @@ export const metadata = {
 
 import Hero from '@/components/hero'
 import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
 import Zigzag from '@/components/zigzag'
-import Testimonials from '@/components/testimonials'
+
 
 export default function Home() {
   return (
@@ -15,8 +14,6 @@ export default function Home() {
       <Hero />
       <Features />
       <Zigzag />
-      <Testimonials />
-      <Newsletter />
     </>
   )
 }
